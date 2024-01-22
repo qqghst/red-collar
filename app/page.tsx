f9df156d93556e622e7bb4593c37f6076ecc6b30
Dmitry O <theicesilver121@mail.ru>
2024-01-22 16:29:19 +0300

    fixed: add dymanic import for components with colorShiftMaterial

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import dynamic from 'next/dynamic';
 import Clients from '@/components/main/clients';
 import Reviews from '@/components/main/reviews';
 import Socials from '@/components/main/socials';
-import Projects from '@/components/main/projects';
+// import Projects from '@/components/main/projects';
 import Services from '@/components/main/services';
 import TwiceBest from '@/components/main/twice-best';
 import Awards from '@/components/main/awards';
@@ -16,6 +16,9 @@ const Home: React.FC = () => {
 	const Masthead = dynamic(() => import('@/components/main/masthead'), {
 		ssr: false,
 		loading: () => <img src='main/masthead/placeholder.png'></img>,
+	});
+	const Projects = dynamic(() => import('@/components/main/projects'), {
+		ssr: false,
 	});
 
 	return (
